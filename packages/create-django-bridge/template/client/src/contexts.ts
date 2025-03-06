7b8c08080ccab0a5c7d6f41aa9433aeb1da7e479
Karl Hobley <karl@kaed.uk>
2025-03-06 10:43:17 +0000

    Delete packages/create-django-bridge directory (#78)
    Moved to separate repository: https://github.com/django-bridge/create-django-bridge

--- packages/create-django-bridge/template/client/src/contexts.ts
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export const CSRFTokenContext = React.createContext<string>("");
--- packages/create-django-bridge/template/server/__projectname__/views.py
+++
@@ -1,5 +0,0 @@
-from django_bridge.response import Response
-
-
-def home(request):
-    return Response(request, "Home", {})
